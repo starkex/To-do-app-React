ecc4bb47dfd872701c2e5c44471f84a8b9fa14a8
Robert Stark <61947842+starkex@users.noreply.github.com>
2020-07-18 19:28:39 +0530

    basic

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, settodos] = useState(["niggas for life", "homies for life"]);
+  const [input, setInput] = useState('');
   return (
     <div className="App">
-         <h1> nigger back to life </h1>
-   
+          <h1> nigger back to life </h1>
+         {todos.map(todos=> <li>{todos}</li> )
+         }
+         <input value={input} onChange={event=>setInput(event.target.value)}/>
+         <button onClick={addto}>Insert it</button>
     </div>
   );
 }
