69cf889e84190e89cb5d25e0daccfd1d043870cb
Robert Stark <61947842+starkex@users.noreply.github.com>
2020-07-19 18:10:36 +0530

    p-tag-button-print

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 function App() {
   const [todos, settodos] = useState(["niggas for life", "homies for life"]);
   const [input, setInput] = useState('');
+  const [pop, setpop] = useState([]);
+  const para = (event)=>{
+    setpop([pop,input]);
+  }
   return (
     <div className="App">
           <h1> nigger back to life </h1>
-         {todos.map(todos=> <li>{todos}</li> )
+         {todos.map(todos=> <li>{todos}</li>)
          }
          <input value={input} onChange={event=>setInput(event.target.value)}/>
-         <button onClick={addto}>Insert it</button>
+         <button onClick={para}>Insert it</button>
+         <p> {pop} </p>
     </div>
   );
 }
