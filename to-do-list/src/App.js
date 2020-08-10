e47bfabc5f55f436654f4fa79378bf93bd721835
Robert Stark <61947842+starkex@users.noreply.github.com>
2020-08-10 16:31:42 +0530

    stat

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [input, setInput] = useState('');
   useEffect(() => {
     Todos.collection('Tasks').orderBy('timestamp','desc').onSnapshot(snapshot =>{
-         settodos(snapshot.docs.map({id: doc.id, todos:doc => doc.data().todos}))
+         settodos(snapshot.docs.map(doc=> ({id: doc.id, todo:doc.data().todos})))
     })
   }, [])
 
@@ -32,14 +32,14 @@ function App() {
           <h1> nigger back to life </h1>
           <ul>
          {todos.map(todos=> 
-         <Wall text={todos}/>
+         <Wall todo={todos}/>
          //<li>{todos}</li>
          )}
-        </ul>
+         </ul>
          <br></br>
          <FormControl>
          <InputLabel> Typr here....</InputLabel>
-         <Input value = { input}onChange = {event => setInput(event.target.value)}/>
+         <Input value = {input}onChange = {event => setInput(event.target.value)}/>
          <br></br>
          <Button disabled = {!input}
          onClick = {para} > Insert it </Button>
--- to-do-list/src/Wall.js
+++ to-do-list/src/Wall.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { List, ListItem, ListItemText, Button } from '@material-ui/core'
+import db from './firebase'
 
 function Wall(props) {
     return(
-                    <List className="sup">
+        <React.Fragment>
+         <List className="sup">
                         <ListItem >
-                           <ListItemText primary="Task" secondary={props.text.doc}/>
+                           <ListItemText primary={props.todo.todo} secondary="Task"/>
                         </ListItem>
                     </List>
-                     <Button className = "yo" > Delete </Button>
-           )
+        <Button onClick={event => {db.collection('todos').doc(props.todos.id).delete()}}> Delete</Button>
+        </React.Fragment>
+    );
+                
+               
 }
 
 export default Wall
