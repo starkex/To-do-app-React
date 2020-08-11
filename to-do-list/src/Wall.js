2b2d833088d0fff2ded72931ab582385f0fbba63
Robert Stark <61947842+starkex@users.noreply.github.com>
2020-08-12 00:35:28 +0530

    complete

--- to-do-list/src/Wall.js
+++ to-do-list/src/Wall.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import { List, ListItem, ListItemText, Button, Modal } from '@material-ui/core'
+import React,{useState} from 'react'
+import { List, ListItem, ListItemText, Button, Modal} from '@material-ui/core'
 import db from './firebase'
 import Todos from './firebase';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles =makeStyles((theme) =>({
+      paper: {
+          width:400,
+          boxShadow: theme.shadows[5],
+          position:'absolute',
+          padding: theme.spacing(2,4,3),
+          backgroundColor:theme.palette.backgroundColor.paper,
+          border:'2px solid #000',
+      },
+}));
 
 function Wall(props) {
+ /* const classes = useStyles();
     const [open, setOpen] = useState(false);
        
     const handleOpen = ()=> {
        setOpen(true);
-    };    
-      const handleClose =() =>{
-          setOpen(false);
-      };
-        return () => {
-            
-           };
-    }, [input])
+    };*/    
+     
     return(
   <React.Fragment>
-        <Modal open={open} onClose={e => setOpen(false)}></Modal>
+        <Modal /*open={open} onClose={e => setOpen(false)}*/>
+          <div>
+            <h1>open</h1>
+             <button /*onClick={e=> setOpen(false)}*/></button>
+          </div>
+        </Modal>
          <List className="sup">
                         <ListItem >
                            <ListItemText primary="Task" secondary={props.text}/>
                         </ListItem>
                     </List>
-        <button onClick={e => Edit }></button>
+       
         </React.Fragment>
     );
                
-}
+};
 
 export default Wall
